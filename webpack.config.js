a81cacba18b97daa49b0fa2acf3381c097bf1fbc
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2023-06-13 00:40:51 +0200

    change /build to /docs

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'main.js'
     },
     devServer: {
