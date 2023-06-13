f714a0077442a99229d8cb3461d187eef73b8c0c
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2023-06-13 16:14:02 +0200

    1. new images 2. images overflow fixed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+console.clear();
 
 const films_list = document.getElementsByClassName("films-list")[0]
 const buttons = document.querySelectorAll("button")
@@ -6,52 +6,65 @@ const tiers = document.getElementsByClassName("tier-content")
 
 let imgSelected = []
 let imgAdded = []
-let filmsNumber = 9
+let filmsNumber = 14
 
+//Create the images
 const fragment = document.createDocumentFragment()
 for (let i = 1; i <= filmsNumber; i++) {
     const img = document.createElement("img")
-    img.setAttribute("src", `./img/film0${i}.jpg`)
+    if (i < 10) {
+        img.setAttribute("src", `./img/film0${i}.jpg`)
+    } else {
+        img.setAttribute("src", `./img/film${i}.jpg`)
+    }
     img.setAttribute("alt", `film0${i}`)
     img.classList.add("pointer")
-    img.addEventListener('click', handleEvent)
+    img.addEventListener('click', handleImg)
     fragment.appendChild(img)
 }
 films_list.appendChild(fragment)
 
+//EventListener in buttons
 buttons.forEach((button) => {
     button.addEventListener('click', handleButton)
 })
 
 
-function handleEvent(event) {
+//AddEventListener IMG
+function handleImg(event) {
     const element = event.currentTarget
     if (element.className == "pointer click") {
         imgSelected.pop(element)
         element.classList.remove("click")
-        message("DESELECTED", element.alt)
+        message("DESELECTED", element.alt, `#fa7970`)
         return
     } else if (element.className == "pointer click added") {
         return
     }
     element.classList.add("click")
     imgSelected.push(element)
-    message("SELECTED", element.alt)
+    message("SELECTED", element.alt, `#77bdfb`)
 }
 
+//AddEventListener BUTTON
 function handleButton(event) {
-    const tierRow = event.currentTarget.title
+    const tierRow = event.currentTarget.value
+    
     if (imgSelected.length == 0) {return}
+
     for (let img of imgSelected) {
         const newItem = document.createElement("img");
+        
         newItem.setAttribute("src", img.src)
         tiers[`${tierRow}`].appendChild(newItem)
         imgSelected = []
         img.classList.add("added")
+
+        message("ADDED", img.alt, `#7ce38b`)
     }
 }
 
-function message(type, message, info='---') {
-    console.log(`[ ${type} ] ${message}`)
-    console.log(info)
+//Print message in the console
+function message(type, message, color='#e6edf3') {
+    console.log(`%c[${type}] ${message}`, `color: ${color}`)
 }
